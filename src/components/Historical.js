4044dcb246c295f78d9c0478aed7abed5f6ec103
Prabhu Marappan <prabhu@pdx.edu>
2022-02-26 14:55:30 -0800

    added code for navbar and empty componnents

---
+++ src/components/Historical.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Historical extends Component {
+  render() {
+    return (
+      <div>Historical</div>
+    )
+  }
+}
+
+export { Historical };
---
+++ src/components/Tweets.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Tweets extends Component {
+  render() {
+    return (
+      <div>Tweets</div>
+    )
+  }
+}
+
+export { Tweets };
